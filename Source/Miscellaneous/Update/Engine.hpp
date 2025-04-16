4620537130e5da6bb530be6c0e87df82c8449dd3
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-04-16 12:26:11 -0700

    Update Engine.hpp

--- Source/Miscellaneous/Update/Engine.hpp
+++ Source/Miscellaneous/Update/Engine.hpp
@@ -18,35 +18,68 @@ namespace Update {
     const uintptr_t GetGlobalStateForInstance = REBASE(0xEF0540);
     const uintptr_t DecryptState = REBASE(0xC92180); // Script Start
     const uintptr_t LuaVM__Load = REBASE(0xC94DA0); // string (oldResult, moduleRef  = ...)
-    const uintptr_t Task__Defer = REBASE(0x10D02D0); // 10D02B0 [Maximum re-entrancy depth (%i) exceeded calling task.defer] or [defer] CatalogSortType
+    const uintptr_t Task__Defer = REBASE(0x10D02D0); // 0x10D02B0 [Maximum re-entrancy depth (%i) exceeded calling task.defer] or [defer] CatalogSortType
     // YUBX::Core Dumper Finished!
+
+
+
+
+    namespace ScriptContext { // Script Start > GetGlobalStateForInstance < DecryptState >
+        const uintptr_t GlobalState = 312;
+        const uintptr_t DecryptState = 0x88;
+    } 
 
     namespace ExtraSpace {
         const uintptr_t Identity = 0x30;
         const uintptr_t Capabilities = 0x48;
     }
 
+
+    //string "WaitingHybridScriptsJob" sub_1AAA560(a1, "WaitingHybridScriptsJob", 1, 0, v7, 7u); >  sub_35AF010(a1, a2, &v14, a6);
+    // sub_35AF010(a1, a2, &v14, a6);
+    //*a1 = &unk_4D91A40;
+    //*(a1 + 432) = a3;
+    //*(a1 + 440) = 0i64;
+    //*(a1 + 448) = a4;
+    //*(a1 + 456) = 0i64;
+    //*(a1 + 464) = 0; JobsStart
+    //*(a1 + 472) = *a5;
+    //*(a1 + 480) = 0;
+    //*(a1 + 456) = sub_35EB150("Jobs", v8, -1, 0, 255);
+    //// 
+    //*(a1 + 88) = 0i64;
+    //*(a1 + 96) = 0;
+    //*(a1 + 104) = 0i64;
+    //*(a1 + 112) = 0i64;
+    //*(a1 + 120) = 0x3FA999999999999Ai64;
+    //*(a1 + 128) = 0i64;
+    //*(a1 + 136) = 0i64;
+    //*(a1 + 144) = 0i64; // JobName
+    //*(a1 + 152) = 1;
+    //v11 = (a1 + 160);
+    //if (dword_5A00FAC > 1)
+    //    sub_36D7120(v11);
+    //else
+    //    sub_36D7030(v11);
+
+
+    //namespace TaskScheduler { // string "Default job arbiter must always be valid"
+    //    const uintptr_t JobsStart = 464;
+    //    const uintptr_t JobName = 144;
+    //}
     namespace TaskScheduler {
         const uintptr_t JobsStart = 0x1D0;
-        const uintptr_t JobName = 0x90;
+        const uintptr_t JobName = 0x18;
     }
-
-    namespace ScriptContext { // Script Start > GetGlobalStateForInstance < DecryptState >
-        const uintptr_t GlobalState = 312;
-        const uintptr_t DecryptState = 136;
-    }
-
-    
-
     namespace DataModel {
-        const uintptr_t FakeDataModelPointer = REBASE(0x61EB418);
+        const uintptr_t FakeDataModelPointer = REBASE(0x6257EB8);
         const uintptr_t FakeDataModelToDataModel = 0x1B8;
-        const uintptr_t PlaceId = 0x178;
+        const uintptr_t PlaceId = 0x180;
         const uintptr_t ScriptContext = 0x3C0;
 
         namespace Information {
-            const uintptr_t PushInstance = REBASE(0xFB7540);
-            const uintptr_t Children = 0x78;
+            const uintptr_t PushInstance = REBASE(0xFBF950);
+            const uintptr_t Children = 0x80;
         }
     }
 }
@@ -91,8 +124,3 @@ namespace RBX {
             SetProto(prototype->p[i], caps);
         }
     }
-
-
-
-
-}
