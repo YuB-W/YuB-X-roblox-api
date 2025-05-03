54d8e47732213bf3bf08f36c46b7b89465de2672
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-05-04 00:57:59 +0200

    Update LuaVM.hpp

--- Source/Miscellaneous/Update/LuaVM.hpp
+++ Source/Miscellaneous/Update/LuaVM.hpp
@@ -1,4 +1,4 @@
-/* Updated for version-c2c9efad42eb44e5 */
+/* Updated for version-a9a5d0b275a34ffb */
 
 #pragma once
 
@@ -7,47 +7,51 @@
 
 #include "Core/Structure.hpp"
 
-#define REBASE(x) x + (uintptr_t)GetModuleHandle(nullptr)
+#define REBASE(x) ((x) + (uintptr_t)GetModuleHandle(nullptr))
 
+// Separators
 #define LUAU_COMMA_SEP ,
 #define LUAU_SEMICOLON_SEP ;
 
-#define LUAU_SHUFFLE3(s, a1, a2, a3) a1 s a2 s a3
-#define LUAU_SHUFFLE4(s, a1, a2, a3, a4) a2 s a4 s a3 s a1
-#define LUAU_SHUFFLE5(s, a1, a2, a3, a4, a5) a3 s a5 s a4 s a2 s a1
-#define LUAU_SHUFFLE6(s, a1, a2, a3, a4, a5, a6) a2 s a4 s a3 s a1 s a5 s a6
-#define LUAU_SHUFFLE7(s, a1, a2, a3, a4, a5, a6, a7) a1 s a2 s a7 s a6 s a4 s a5 s a3
-#define LUAU_SHUFFLE8(s, a1, a2, a3, a4, a5, a6, a7, a8) a4 s a3 s a2 s a7 s a6 s a5 s a8 s a1
-#define LUAU_SHUFFLE9(s, a1, a2, a3, a4, a5, a6, a7, a8, a9) a4 s a5 s a9 s a8 s a7 s a6 s a1 s a3 s a2
+// Shuffle macros
+#define LUAU_SHUFFLE2(s,a0,a1) a0 s a1
+#define LUAU_SHUFFLE3(s,a0,a1,a2) a0 s a1 s a2
+#define LUAU_SHUFFLE4(s,a0,a1,a2,a3) a2 s a1 s a3 s a0
+#define LUAU_SHUFFLE5(s,a0,a1,a2,a3,a4) a1 s a3 s a4 s a0 s a2
+#define LUAU_SHUFFLE6(s,a0,a1,a2,a3,a4,a5) a2 s a4 s a3 s a0 s a5 s a1
+#define LUAU_SHUFFLE7(s,a0,a1,a2,a3,a4,a5,a6) a5 s a1 s a6 s a2 s a0 s a3 s a4
+#define LUAU_SHUFFLE8(s,a0,a1,a2,a3,a4,a5,a6,a7) a0 s a3 s a4 s a2 s a7 s a1 s a6 s a5
+#define LUAU_SHUFFLE9(s,a0,a1,a2,a3,a4,a5,a6,a7,a8) a6 s a3 s a5 s a8 s a0 s a7 s a1 s a4 s a2
 
-#define PROTO_MEMBER1_ENC VMValue3
-#define PROTO_MEMBER2_ENC VMValue2
-#define PROTO_DEBUGISN_ENC VMValue4
-#define PROTO_TYPEINFO_ENC VMValue1
-#define PROTO_DEBUGNAME_ENC VMValue3
+// VMValue encodings
+#define PROTO_MEMBER1_ENC     vmval0
+#define PROTO_MEMBER2_ENC     vmval2
+#define PROTO_DEBUGISN_ENC    vmval4
+#define PROTO_TYPEINFO_ENC    vmval1
+#define PROTO_DEBUGNAME_ENC   vmval3
 
-#define LSTATE_STACKSIZE_ENC VMValue4
-#define LSTATE_GLOBAL_ENC VMValue4
+#define LSTATE_STACKSIZE_ENC  vmval4
+#define LSTATE_GLOBAL_ENC     vmval0
 
-#define CLOSURE_FUNC_ENC VMValue3
-#define CLOSURE_CONT_ENC VMValue4
-#define CLOSURE_DEBUGNAME_ENC VMValue2
+#define CLOSURE_FUNC_ENC      vmval0
+#define CLOSURE_CONT_ENC      vmval4
+#define CLOSURE_DEBUGNAME_ENC vmval2
 
-#define TABLE_MEMBER_ENC VMValue3
-#define TABLE_META_ENC VMValue3
+#define TABLE_MEMBER_ENC      vmval0
+#define TABLE_META_ENC        vmval0
+#define UDATA_META_ENC        vmval1
 
-#define UDATA_META_ENC VMValue1
+#define TSTRING_HASH_ENC      vmval2
+#define TSTRING_LEN_ENC       vmval0
 
-#define TSTRING_HASH_ENC VMValue2
-#define TSTRING_LEN_ENC VMValue1
+#define GSTATE_TTNAME_ENC     vmval0
+#define GSTATE_TMNAME_ENC     vmval0
 
-#define GSTATE_TTNAME_ENC VMValue1
-#define GSTATE_TMNAME_ENC VMValue1
 
 namespace Update {
     namespace LuaVM {
-        const uintptr_t LuaO_NilObject = REBASE(0x45FE5B8);
-        const uintptr_t LuaH_DummyNode = REBASE(0x45FDFD8);
-        const uintptr_t Luau_Execute = REBASE(0x2693AC0);
+        const uintptr_t LuaO_NilObject = REBASE(0x466CDA8);
+        const uintptr_t LuaH_DummyNode = REBASE(0x466C7C8); 
+        const uintptr_t Luau_Execute = REBASE(0x26F63B0); 
     }
 }
