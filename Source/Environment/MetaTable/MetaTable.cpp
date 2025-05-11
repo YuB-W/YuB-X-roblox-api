d1c3b18a087f62d23ebca27947b5a6d930b1ef35
BaconWareMain <164041817+BaconWareMain@users.noreply.github.com>
2025-05-11 00:26:51 -0700

    Update MetaTable.cpp

--- Source/Environment/MetaTable/MetaTable.cpp
+++ Source/Environment/MetaTable/MetaTable.cpp
@@ -5,39 +5,50 @@
 #define RegisterFunction(L, Func, Name) lua_pushcclosure(L, Func, Name, 0); \
 lua_setglobal(L, Name);
 
+int custom_getrawmetatable(lua_State* L) {
+	luaL_checkany(L, 1);
 
-int GetRawMetaTable(lua_State* L)
-{
-	lua_getmetatable(L, 1);
-	return 1; // Return the table
+	if (!lua_getmetatable(L, 1))
+		lua_pushnil(L);
+
+	return 1;
 }
+int custom_setrawmetatable(lua_State* L) {
+	luaL_checkany(L, 1);
+	luaL_checktype(L, 2, LUA_TTABLE);
 
-int SetRawMetaTable(lua_State* L)
-{
 	lua_setmetatable(L, 1);
-	return 0; // Return the table
-}
+	lua_pushvalue(L, 1);
 
-int SetReadOnly(lua_State* L)
-{
-	lua_setreadonly(L, 1, lua_toboolean(L, 2));
-	return 0;
-}
-
-int IsReadOnly(lua_State* L)
-{
-	luaL_checktype(L, 1, LUA_TTABLE);
-
-	lua_pushboolean(L, lua_getreadonly(L, 1));
 	return 1;
 }
 
+
+auto setreadonly(lua_State* rl) -> int {
+	luaL_checktype(rl, 2, LUA_TBOOLEAN);
+	lua_setreadonly(rl, 1, lua_toboolean(rl, 2));
+	return 0;
+}
+
+
+int custom_isreadonly(lua_State* L) {
+	luaL_checktype(L, 1, LUA_TTABLE);
+
+	lua_pushboolean(L, hvalue(luaA_toobject(L, 1))->readonly);
+	return 1;
+};
+
+
 void api::Environment::MetaTable::Register(lua_State* L)
 {
-	RegisterFunction(L, GetRawMetaTable, "getrawmetatable");
-	RegisterFunction(L, SetRawMetaTable, "setrawmetatable");
-	RegisterFunction(L, SetReadOnly, "setreadonly");
-	RegisterFunction(L, IsReadOnly, "isreadonly");
+
+	RegisterFunction(L, custom_getrawmetatable, "getrawmetatable");
+	RegisterFunction(L, custom_setrawmetatable, "setrawmetatable");
+
+
+	RegisterFunction(L, custom_setrawmetatable, "setrawmetatable");
+	RegisterFunction(L, setreadonly, "setreadonly");
+	RegisterFunction(L, custom_isreadonly, "isreadonly");
 
 
 }
